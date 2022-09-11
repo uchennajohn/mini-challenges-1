fe5323dbcb256b229d42e1f21d136f7d2a5814ae
Uchenna <okechukwu@gmail.com>
2022-09-11 12:39:51 +0100

    worked on mini challenge one

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,47 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
 
+
+
+
+
+function isValid(str) {
+ let bracketStr = [];
+ let strArr = str.split('');
+ let firstStrIndex = str[0]; 
+
+ if(firstStrIndex === ')' || firstStrIndex=== '}' || firstStrIndex === ']') {
+    return 'invalid'
+ }else{
+
+    for(let i = 0; i< strArr.length; i++) {
+        //const bracket = strArr[i]
+
+        if(strArr[i] ==='{' || strArr[i] === '(' || strArr[i] === '[') {
+            bracketStr.push(strArr[i])
+            }else{
+
+                    if(strArr[strArr.length-1] ==='{' && strArr[i] === '}'){
+                        bracketStr.pop()
+                    }
+                    if(strArr[strArr.length-1] === '[' && strArr[i] ===']'){
+                        bracketStr.pop()
+                    }
+                   if(strArr[strArr.length-1] === '(' && strArr[i] ===')') {
+                        bracketStr.pop()
+                    }
+            }
+
+    }
+ }
+    if(bracketStr.length == 0){
+        return "valid"
+    }else{
+        return "invalid"
+    }
+}
+        
 module.exports = isValid;
+console.log(isValid("{{{{{{{{{{{{}}}}}}}}}}}()[}]"))
+console.log(isValid("(){}[]"))
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -1,9 +1,27 @@
-/**
+  /**
  * Implement the solution in this function
  *
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let answer = [];
+    let temp = [];
+    let x = 0;
+    for (let index = 0; index < array.length; index++) {
+        const i = array[index];
+        if(x < i.length){
+            temp.push(i[x])
+        }
+        if(index == array.length - 1 && x < i.length ){
+            answer.push(temp);
+            temp = []
+            
+            x += 1
+            index = -1
+        }
+    }
+    return answer
+}
 
 module.exports = transpose;
